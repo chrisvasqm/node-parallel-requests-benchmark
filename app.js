d61a5141cf947406481bf84bcabb0787c6d0d13a
chrisvasqm <christianvasquezmelo@gmail.com>
2026-03-03 23:54:01 -0400

    Cleanup code

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.json({ message: 'Welcome to the Express server!' });
 });
 
-// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
